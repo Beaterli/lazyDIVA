8da8ee879fd26164f6def414823840651df56d33
presisco <internight@sina.com>
2019-08-25 11:24:23 +0800

    use tensorflow cvae example

--- vae.py
+++ vae.py
@@ -1,7 +1,43 @@
+
 from __future__ import absolute_import, division, print_function
 
 # Import TensorFlow >= 1.9 and enable eager execution
 import tensorflow as tf
+
+tfe = tf.contrib.eager
+tf.enable_eager_execution()
+
+import os
+import time
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+import PIL
+import imageio
+from IPython import display
+
+(train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
+
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1).astype('float32')
+
+# Normalizing the images to the range of [0., 1.]
+train_images /= 255.
+test_images /= 255.
+
+# Binarization
+train_images[train_images >= .5] = 1.
+train_images[train_images < .5] = 0.
+test_images[test_images >= .5] = 1.
+test_images[test_images < .5] = 0.
+
+TRAIN_BUF = 60000
+BATCH_SIZE = 100
+
+TEST_BUF = 10000
+
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+test_dataset = tf.data.Dataset.from_tensor_slices(test_images).shuffle(TEST_BUF).batch(BATCH_SIZE)
 
 
 class CVAE(tf.keras.Model):
@@ -96,3 +132,76 @@ def apply_gradients(optimizer, gradients, variables, global_step=None):
     optimizer.apply_gradients(zip(gradients, variables), global_step=global_step)
 
 
+epochs = 100
+latent_dim = 50
+num_examples_to_generate = 16
+
+# keeping the random vector constant for generation (prediction) so
+# it will be easier to see the improvement.
+random_vector_for_generation = tf.random_normal(
+    shape=[num_examples_to_generate, latent_dim])
+model = CVAE(latent_dim)
+
+
+def generate_and_save_images(model, epoch, test_input):
+    predictions = model.sample(test_input)
+    fig = plt.figure(figsize=(4, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(predictions[i, :, :, 0], cmap='gray')
+        plt.axis('off')
+
+    # tight_layout minimizes the overlap between 2 sub-plots
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    plt.show()
+
+
+generate_and_save_images(model, 0, random_vector_for_generation)
+
+for epoch in range(1, epochs + 1):
+    start_time = time.time()
+    for train_x in train_dataset:
+        gradients, loss = compute_gradients(model, train_x)
+        apply_gradients(optimizer, gradients, model.trainable_variables)
+    end_time = time.time()
+
+    if epoch % 1 == 0:
+        loss = tfe.metrics.Mean()
+        for test_x in test_dataset:
+            loss(compute_loss(model, test_x))
+        elbo = -loss.result()
+        display.clear_output(wait=False)
+        print('Epoch: {}, Test set ELBO: {}, '
+              'time elapse for current epoch {}'.format(epoch,
+                                                        elbo,
+                                                        end_time - start_time))
+        generate_and_save_images(
+            model, epoch, random_vector_for_generation)
+
+
+def display_image(epoch_no):
+    return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+
+
+display_image(epochs)  # Display images
+
+with imageio.get_writer('cvae.gif', mode='I') as writer:
+    filenames = glob.glob('image*.png')
+    filenames = sorted(filenames)
+    last = -1
+    for i, filename in enumerate(filenames):
+        frame = 2 * (i ** 0.5)
+        if round(frame) > round(last):
+            last = frame
+        else:
+            continue
+        image = imageio.imread(filename)
+        writer.append_data(image)
+    image = imageio.imread(filename)
+    writer.append_data(image)
+
+# this is a hack to display the gif inside the notebook
+os.system('cp cvae.gif cvae.gif.png')
+
+display.Image(filename="cvae.gif.png")
