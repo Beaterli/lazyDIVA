fix path finder gradient calculation

pathfinder/lstmfinder.py
@@ -94,12 +94,13 @@ class LSTMFinder(tf.keras.Model):
         step = 0
         paths = [(from_id,)]
         ent_of_paths = [(from_id,)]
+        action_prob_stacks = [()]
+        action_chosens = [()]
 
         initial_input = np.zeros(self.emb_size, dtype="f4")
         # [0]是hidden state, [1]是carry state
         initial_state = self.history_stack.get_initial_state(inputs=np.expand_dims(initial_input, axis=0))
         history_stack_states = [(initial_input, initial_state[0], initial_state[1])]
-        action_prob_stacks = [()]
 
         # 最大搜索history_depth跳
         while step < self.history_depth:
@@ -109,6 +110,7 @@ class LSTMFinder(tf.keras.Model):
             updated_stack_states = []
             updated_ent_of_paths = []
             updated_action_prob_stacks = []
+            updated_action_chosens = []
 
             for index, path in enumerate(paths):
                 # 跳过到达目的地的路径
@@ -117,6 +119,7 @@ class LSTMFinder(tf.keras.Model):
                     updated_stack_states.append(history_stack_states[index])
                     updated_ent_of_paths.append(ent_of_paths[index])
                     updated_action_prob_stacks.append(action_prob_stacks[index])
+                    updated_action_chosens.append(action_chosens[index])
                     continue
 
                 # 选择邻接矩阵
@@ -147,12 +150,14 @@ class LSTMFinder(tf.keras.Model):
                 ent_of_path = ent_of_paths[index[0]]
                 next_step = all_candidates[index[0]][index[1]]
                 action_prob_stack = action_prob_stacks[index[0]]
+                action_chosen = action_chosens[index[0]]
 
                 # 添加新的top n的路径信息
                 updated_paths.append(path + (next_step.rel_id, next_step.to_id))
                 updated_ent_of_paths.append(ent_of_path + (next_step.to_id,))
                 updated_stack_states.append(history_stack_states[index[0]])
                 updated_action_prob_stacks.append(action_prob_stack + (all_probabilities[index[0]],))
+                updated_action_chosens.append(action_chosen + (index[1],))
 
                 # 计算新的top n的LSTM 状态
                 input_vector = np.concatenate(
@@ -170,6 +175,7 @@ class LSTMFinder(tf.keras.Model):
             history_stack_states = updated_stack_states
             ent_of_paths = updated_ent_of_paths
             action_prob_stacks = updated_action_prob_stacks
+            action_chosens = updated_action_chosens
             step = step + 1
 
-        return paths, action_prob_stacks
+        return paths, action_prob_stacks, action_chosens

train.py
@@ -65,16 +65,16 @@ def posterior_update(posterior_loss, tape):
     optimizer.apply_gradients(zip(gradients, path_finder.trainable_variables))
 
 
-def mdp_reinforce_update(reason_loss, action_possibilities, tape):
-    for step_possibilities in action_possibilities:
+def mdp_reinforce_update(reason_loss, actions, action_possibilities, tape):
+    for step_index, step_possibilities in enumerate(action_possibilities):
         action_dim = step_possibilities.shape[0]
-        action_onehot = tf.cast(tf.one_hot(step_possibilities, action_dim), tf.bool)
-        picked_action = tf.boolean_mask(step_possibilities, action_onehot)
+        action_onehot = tf.one_hot(actions[step_index], action_dim)
+        action_mask = tf.cast(action_onehot, tf.bool)
+        picked_action = tf.boolean_mask(step_possibilities, action_mask)
         reward = tf.reduce_sum(-tf.log(picked_action) * reason_loss)
 
         gradients = tape.gradient(reward, path_finder.trainable_variables)
         optimizer.apply_gradients(zip(gradients, path_finder.trainable_variables))
-        reason_loss = reason_loss * reward_param
     return
 
 
@@ -97,14 +97,16 @@ for i in range(epoch):
 
         # 从posterior rollout K个路径
         with tf.GradientTape() as gradient_tape:
-            paths, action_possibilities = path_finder.paths_between(episode['from_id'], episode['to_id'], rel_emb, 3)
+            paths, action_possibilities, action_chosens = path_finder.paths_between(episode['from_id'],
+                                                                                    episode['to_id'], rel_emb, 3)
             print("Paths: " + str(paths))
 
             # Monte-Carlo REINFORCE奖励计算
             log_pr = 0.0
             for path_index, path in enumerate(paths):
                 path_loss = reasoner_loss(path_reasoner.relation_of_path(path), label)
-                mdp_reinforce_update(path_loss, action_possibilities[path_index], gradient_tape)
+                mdp_reinforce_update(path_loss, action_chosens[path_index], action_possibilities[path_index],
+                                     gradient_tape)
                 log_pr += path_loss
             log_pr = log_pr / len(paths)
 